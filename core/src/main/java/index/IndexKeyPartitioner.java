4f3174f19a2ca8881faecac4a9748bf384f7e40a
Cheng-Yu Hsu <m@cyhsu.me>
2016-04-16 16:41:20 +0800

    ..

--- core/src/main/java/index/IndexKeyPartitioner.java
+++ core/src/main/java/index/IndexKeyPartitioner.java
@@ -8,9 +8,9 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class IndexKeyPartitioner extends
 		Partitioner<IndexKey, TermFrequencyWritable> {
 	private static final List<Character> CHAR_RANGE = Arrays.asList('0', '1',
-			'2', '3', '4', '5', '6', '7', '8', '9', '-', 'a', 'b', 'c', 'd',
-			'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-			'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+			'2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
+			'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+			's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 
 	@Override
 	public int getPartition(IndexKey key, TermFrequencyWritable value,
